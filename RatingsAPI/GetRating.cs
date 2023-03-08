2b4f50124946b06404b4f2323f54de84332f4c68
unknown <harigovindan.gs@gmail.com>
2023-03-08 09:21:42 +0000

    GetRating

---
+++ RatingsAPI/GetRating.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace RatingsAPI
+{
+    public class GetRating
+    {
+        private readonly ILogger _logger;
+
+        public GetRating(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<GetRatings>();
+        }
+
+        [Function("GetRating")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,String ratingId)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+                      
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Get Rating for !"+ratingId);
+
+            return response;
+        }
+    }
+}
