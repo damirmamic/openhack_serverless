39d8128c9305abd136904ffbf04301939cc30d02
vladimirstokic <vladimir.stokic@gmail.com>
2023-03-08 09:37:31 +0100

    Added Rating Class

---
+++ RatingsAPI/ModelClasses/Rating.cs
@@ -0,0 +1,13 @@
+﻿namespace RatingsAPI.ModelClasses
+{
+    internal class Rating
+    {
+        public string id { get; set; }
+        public string userId { get; set; }
+        public string productId { get; set; }
+        public DateTime timestamp { get; set; }
+        public string locationName { get; set; }
+        public int rating { get; set; }
+        public string userNotes { get; set; }
+    }
+}
